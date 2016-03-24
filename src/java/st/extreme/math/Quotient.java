3bc478c050be60dcddab6881ae0377c4d306c58a
Otmar Humbel <ohumbel@gmail.com>
2016-03-24 15:32:57 +0100

    valueOf(String), sign

--- src/java/st/extreme/math/Quotient.java
+++ src/java/st/extreme/math/Quotient.java
@@ -3,15 +3,54 @@ package st.extreme.math;
 public class Quotient {
 
 	private static final String ONE = "1";
+	private static final String ZERO = "0";
+
+	private static final char MINUS_CHAR = '-';
+	private static final char PLUS_CHAR = '+';
+	private static final char DOT_CHAR = '.';
+	private static final char D0_CHAR = '0';
+	private static final char D1_CHAR = '1';
+	private static final char D2_CHAR = '2';
+	private static final char D3_CHAR = '3';
+	private static final char D4_CHAR = '4';
+	private static final char D5_CHAR = '5';
+	private static final char D6_CHAR = '6';
+	private static final char D7_CHAR = '7';
+	private static final char D8_CHAR = '8';
+	private static final char D9_CHAR = '9';
 
 	private String numerator;
 	private String denominator;
+	private boolean positive;
 
+	/**
+	 * create a positive quotient
+	 * 
+	 * @param numerator
+	 * @param denominator
+	 */
 	Quotient(String numerator, String denominator) {
-		this.numerator = numerator;
-		this.denominator = denominator;
+		this(numerator, denominator, true);
 	}
 
+	/**
+	 * create a quotient with a sign
+	 * 
+	 * @param numerator
+	 * @param denominator
+	 * @param positive
+	 */
+	Quotient(String numerator, String denominator, boolean positive) {
+		this.numerator = numerator;
+		this.denominator = denominator;
+		this.positive = positive;
+	}
+
+	/**
+	 * Create a quotient with the reciprocal value
+	 * 
+	 * @return a new quotient with the reciprocal value of this quotient
+	 */
 	public Quotient reciprocal() {
 		return new Quotient(denominator, numerator);
 	}
@@ -24,11 +63,90 @@ public class Quotient {
 		return denominator;
 	}
 
-	public String toString() {
-		return numerator.concat("/").concat(denominator);
+	public boolean isPositive() {
+		return positive;
 	}
 
-	public static Quotient fromString(String number) {
-		return new Quotient(number, ONE);
+	/**
+	 * @return a human readable representation
+	 */
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		if (!isPositive()) {
+			builder.append(MINUS_CHAR);
+		}
+		builder.append(numerator);
+		builder.append('/');
+		builder.append(denominator);
+		return builder.toString();
+	}
+
+	/**
+	 * Create a new quotient from a string input
+	 * 
+	 * @param number
+	 *            in the format [sign][digits].[digits]
+	 * 
+	 * @return a quotient representing the passed in number value
+	 */
+	public static Quotient valueOf(String number) {
+		if (number == null || number.isEmpty() || ZERO.equals(number)) {
+			return new Quotient(ZERO, ONE);
+		}
+		char[] chars = number.toCharArray();
+		boolean isPositive = true;
+		StringBuilder numerator = new StringBuilder();
+		StringBuilder denominator = new StringBuilder();
+		denominator.append(ONE);
+		int pos = 0;
+		boolean beforeDecimal = true;
+		for (char c : chars) {
+			switch (c) {
+			case MINUS_CHAR:
+				isPositive = false;
+				// intentially no break
+			case PLUS_CHAR:
+				if (pos > 0) {
+					throw new NumberFormatException("illegal character ".concat(String.valueOf(c))
+							.concat(" at position ").concat(String.valueOf(pos)));
+				}
+				break;
+			case DOT_CHAR:
+				beforeDecimal = false;
+				break;
+			case D0_CHAR:
+				if (numerator.length() == 0 && beforeDecimal) {
+					// skip leading zeroes
+					break;
+				}
+				// intentially no break
+			case D1_CHAR:
+			case D2_CHAR:
+			case D3_CHAR:
+			case D4_CHAR:
+			case D5_CHAR:
+			case D6_CHAR:
+			case D7_CHAR:
+			case D8_CHAR:
+			case D9_CHAR:
+				numerator.append(c);
+				if (!beforeDecimal) {
+					denominator.append(ZERO);
+				}
+				break;
+			default:
+				throw new NumberFormatException("illegal character ".concat(String.valueOf(c)).concat(" at position ")
+						.concat(String.valueOf(pos)));
+			}
+			pos++;
+		}
+		if (numerator.length() == 0) {
+			numerator.append(ZERO);
+		}
+		if (ZERO.equals(numerator.toString())) {
+			return new Quotient(numerator.toString(), ONE, isPositive);
+		} else {
+			return new Quotient(numerator.toString(), denominator.toString(), isPositive);
+		}
 	}
 }
--- test/java/st/extreme/math/QuotientTest.java
+++ test/java/st/extreme/math/QuotientTest.java
@@ -2,7 +2,6 @@ package st.extreme.math;
 
 import static org.junit.Assert.*;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class QuotientTest {
@@ -13,30 +12,242 @@ public class QuotientTest {
 		Quotient r = q.reciprocal();
 		assertEquals("4", r.getNumerator());
 		assertEquals("3", r.getDenominator());
+		assertEquals(q.isPositive(), r.isPositive());
 	}
 
 	@Test
 	public void testToString() {
 		Quotient q = new Quotient("3", "4");
 		assertEquals("3/4", q.toString());
+		q = new Quotient("3", "4", false);
+		assertEquals("-3/4", q.toString());
 	}
 
 	@Test
-	public void testFromString_Integer() {
-		Quotient q = Quotient.fromString("0");
+	public void testValueOf_Integer() {
+		Quotient q = Quotient.valueOf("0");
 		assertEquals("0", q.getNumerator());
 		assertEquals("1", q.getDenominator());
-		q = Quotient.fromString("1");
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("1");
 		assertEquals("1", q.getNumerator());
 		assertEquals("1", q.getDenominator());
-		q = Quotient.fromString("-1");
-		assertEquals("-1", q.getNumerator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("+1");
+		assertEquals("1", q.getNumerator());
 		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("-1");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertFalse(q.isPositive());
 	}
 
 	@Test
-	@Ignore
-	public void testFromString_WithDecimals() {
-		fail("implement");
+	public void testValueOf_noString() {
+		Quotient q = Quotient.valueOf("");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf(null);
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+	}
+
+	@Test
+	public void testValueOf_Signs() {
+		Quotient q = Quotient.valueOf("-1");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertFalse(q.isPositive());
+
+		q = Quotient.valueOf("+1");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+	}
+
+	@Test
+	public void testValueOf_LeadingZeroes() {
+		Quotient q = Quotient.valueOf("000");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("001");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("+001");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("-001");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertFalse(q.isPositive());
+	}
+
+	@Test
+	public void testValueOf_WithDecimals() {
+		Quotient q = Quotient.valueOf("1.5");
+		assertEquals("15", q.getNumerator());
+		assertEquals("10", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("-1.5");
+		assertEquals("15", q.getNumerator());
+		assertEquals("10", q.getDenominator());
+		assertFalse(q.isPositive());
+
+		q = Quotient.valueOf("-1234.5678");
+		assertEquals("12345678", q.getNumerator());
+		assertEquals("10000", q.getDenominator());
+		assertFalse(q.isPositive());
+
+		// maybe we could eliminate the same number of trailing zeroes
+		q = Quotient.valueOf("1.000");
+		assertEquals("1000", q.getNumerator());
+		assertEquals("1000", q.getDenominator());
+		assertTrue(q.isPositive());
+	}
+
+	@Test
+	public void testValueOf_MinimalInput() {
+		Quotient q = Quotient.valueOf(".5");
+		assertEquals("5", q.getNumerator());
+		assertEquals("10", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf(".");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf(".0");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf(".1");
+		assertEquals("1", q.getNumerator());
+		assertEquals("10", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("0.");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("1.");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("-");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertFalse(q.isPositive());
+
+		q = Quotient.valueOf("-0");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertFalse(q.isPositive());
+
+		q = Quotient.valueOf("-1");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertFalse(q.isPositive());
+
+		q = Quotient.valueOf("+");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("+0");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("+1");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("+.");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("+.0");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("+.1");
+		assertEquals("1", q.getNumerator());
+		assertEquals("10", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("+0.");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("+1.");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertTrue(q.isPositive());
+
+		q = Quotient.valueOf("-.");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertFalse(q.isPositive());
+
+		q = Quotient.valueOf("-.0");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertFalse(q.isPositive());
+
+		q = Quotient.valueOf("-.1");
+		assertEquals("1", q.getNumerator());
+		assertEquals("10", q.getDenominator());
+		assertFalse(q.isPositive());
+
+		q = Quotient.valueOf("-0.");
+		assertEquals("0", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertFalse(q.isPositive());
+
+		q = Quotient.valueOf("-1.");
+		assertEquals("1", q.getNumerator());
+		assertEquals("1", q.getDenominator());
+		assertFalse(q.isPositive());
+	}
+
+	@Test
+	public void testValueOf_IllegalInput() {
+		assertNumberFormatException("x");
+		assertNumberFormatException(".-");
+		assertNumberFormatException(".+");
+		assertNumberFormatException("1x");
+		assertNumberFormatException(".2-");
+		assertNumberFormatException(".3+");
+	}
+
+	private void assertNumberFormatException(String input) {
+		try {
+			Quotient.valueOf(input);
+			fail("NumberFormatException expected");
+		} catch (NumberFormatException nfe) {
+			// ok
+		}
 	}
 }
